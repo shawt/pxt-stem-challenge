9977a096eb5f106d6ace252aab02394891104b5e
Trevor Shaw <shawt@genlrn.com>
2021-05-07 13:29:32 -0400

    Update gamerbit.ts

--- gamerbit.ts
+++ gamerbit.ts
@@ -3,7 +3,7 @@
  */
 //%
 enum GamerBitPin {
-    //% block="P0 (D-PAD up-ts)"
+    //% block="P0 (D-PAD up)"
     P0 = DAL.MICROBIT_ID_IO_P0,
     //% block="P1 (D-PAD left)"
     P1 = DAL.MICROBIT_ID_IO_P1,
